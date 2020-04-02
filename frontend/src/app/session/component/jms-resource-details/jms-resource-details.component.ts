8e10ab5fd456d3907b3aa21825fe8ee29b90451a
Paul Sterl <paul.sterl@outlook.de>
2020-04-02 23:17:11 +0200

    added refresh button for jms resource details

--- frontend/src/app/session/component/jms-resource-details/jms-resource-details.component.ts
+++ frontend/src/app/session/component/jms-resource-details/jms-resource-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject, Input } from '@angular/core';
 import { JmsSessionService } from '../../service/session/jms-session.service';
 import { JmsResourceType, JmsResource } from 'src/app/api/jms-session';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
 
 export interface JmsResourceDetailData {
     connectorId: number;
@@ -25,6 +26,9 @@ export class JmsResourceDetailsComponent implements OnInit {
     @Input() connectorId: number;
     @Input() resource: JmsResource;
 
+    error: any;
+    private readonly isLoading = new BehaviorSubject(false);
+    loading$ = this.isLoading.asObservable();
     queueData: CacheEntry;
     constructor(private sessionService: JmsSessionService) { }
 
@@ -32,18 +36,29 @@ export class JmsResourceDetailsComponent implements OnInit {
         if (this.resource) {
             this.queueData = JmsResourceDetailsComponent.CACHE.get(this.connectorId + this.resource.name);
             if (this.queueData == null) {
-                if (this.resource.type === JmsResourceType.QUEUE) {
-                    this.sessionService.getQueueInfo(this.connectorId, this.resource.name).subscribe(queueData => {
-                        this.queueData = {date: new Date(), data: queueData};
-                        JmsResourceDetailsComponent.CACHE.set(this.connectorId + this.resource.name, this.queueData);
-                    });
-                } else {
-                    this.sessionService.getTopicInfo(this.connectorId, this.resource.name).subscribe(queueData => {
-                        this.queueData = {date: new Date(), data: queueData};
-                        JmsResourceDetailsComponent.CACHE.set(this.connectorId + this.resource.name, this.queueData);
-                    });
-                }
+                this.doLoad();
             }
+        }
+    }
+
+    doLoad() {
+        this.error = null;
+        this.isLoading.next(true);
+        if (this.resource.type === JmsResourceType.QUEUE) {
+                this.sessionService.getQueueInfo(this.connectorId, this.resource.name).subscribe(queueData => {
+                    this.queueData = {date: new Date(), data: queueData};
+                    JmsResourceDetailsComponent.CACHE.set(this.connectorId + this.resource.name, this.queueData);
+                },
+                e => this.error = e.error || e,
+                () => this.isLoading.next(false)
+            );
+        } else {
+            this.sessionService.getTopicInfo(this.connectorId, this.resource.name).subscribe(queueData => {
+                this.queueData = {date: new Date(), data: queueData};
+                JmsResourceDetailsComponent.CACHE.set(this.connectorId + this.resource.name, this.queueData);
+            },
+            e => this.error = e.error || e,
+            () => this.isLoading.next(false));
         }
     }
 }
